9759460caeb403308cf6c46e4116340e8545e0b0
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-22 16:47:17 +0200

    ADD: Turn on and off bonus exercise

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
+// const lamp = document.querySelector(".lamp");
+// const buttonOnOff = document.getElementById("btn");
+
+// buttonOnOff.addEventListener("click", function(){
+//     lamp.src = "./img/yellow_lamp.png";
+//     lamp.classList.add("lamp-on");
+//     buttonOnOff.innerHTML = "Spegni";
+// })
+
+
 const lamp = document.querySelector(".lamp");
 const buttonOnOff = document.getElementById("btn");
 
-buttonOnOff.addEventListener("click", function(){
-    lamp.src = "./img/yellow_lamp.png";
-    lamp.classList.add("lamp-on");
-    buttonOnOff.innerHTML = "Spegni";
+buttonOnOff.addEventListener("click", function () {
+    if (lamp.src.includes("white_lamp.png")) {
+        lamp.src = "./img/yellow_lamp.png";
+        lamp.classList.add("lamp-on");
+        buttonOnOff.innerHTML = "Spegni";
+    } else {
+        lamp.src = "./img/white_lamp.png";
+        buttonOnOff.innerHTML = "Accendi";
+    }
 })
