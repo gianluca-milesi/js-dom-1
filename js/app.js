ac4f97e1495b2a19855ff9c6fbdb83e945b2c799
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-22 16:37:47 +0200

    ADD: event listener function on button. Turn on the light

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log("ciao")
+const lamp = document.querySelector(".lamp");
+const buttonOnOff = document.getElementById("btn");
+
+buttonOnOff.addEventListener("click", function(){
+    lamp.src = "./img/yellow_lamp.png";
+    lamp.classList.add("lamp-on");
+    buttonOnOff.innerHTML = "Spegni";
+})
