e5e7ef88a485ba4c0f25807ed69f85dd6aedebd3
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2024-10-22 18:14:30 +0200

    ADD: bootstrap lamp card

--- js/app.js
+++ js/app.js
@@ -8,36 +8,36 @@
 //     buttonOnOff.innerHTML = "Spegni";
 // })
 
-//Esercizio Bonus
-// const lamp = document.querySelector(".lamp");
-// const buttonOnOff = document.getElementById("btn");
 
-// buttonOnOff.addEventListener("click", function () {
-//     if (lamp.src.includes("./img/white_lamp.png")) {
-//         lamp.src = "./img/yellow_lamp.png";
-//         lamp.classList.add("lamp-on");
-//         buttonOnOff.innerHTML = "Spegni";
-//     } else {
-//         lamp.src = "./img/white_lamp.png";
-//         buttonOnOff.innerHTML = "Accendi";
-//     }
-// })
-
-
-
-
-//Metodo 2
-const lampOff = document.querySelector(".white-lamp");
-const lampOn = document.querySelector(".yellow-lamp");
-lampOn.classList.add("display-none");
+// Esercizio Bonus
+const lamp = document.querySelector(".lamp-img");
 const buttonOnOff = document.getElementById("btn");
 
 buttonOnOff.addEventListener("click", function () {
-    if (lampOn.classList.contains("display-none")) {
-        lampOn.classList.toggle("display-none");
-        lampOff.classList.add("display-none");
+    if (lamp.src.includes("white_lamp.png")) {
+        lamp.src = "./img/yellow_lamp.png";
+        buttonOnOff.innerHTML = "Spegni";
     } else {
-        lampOn.classList.add("display-none");
-        lampOff.classList.toggle("display-none");
+        lamp.src = "./img/white_lamp.png";
+        buttonOnOff.innerHTML = "Accendi";
     }
 })
+
+
+
+
+// //Metodo 2
+// const lampOff = document.querySelector(".white-lamp");
+// const lampOn = document.querySelector(".yellow-lamp");
+// lampOn.classList.add("display-none");
+// const buttonOnOff = document.getElementById("btn");
+
+// buttonOnOff.addEventListener("click", function () {
+//     if (lampOn.classList.contains("display-none")) {
+//         lampOn.classList.toggle("display-none");
+//         lampOff.classList.add("display-none");
+//     } else {
+//         lampOn.classList.add("display-none");
+//         lampOff.classList.toggle("display-none");
+//     }
+// })
